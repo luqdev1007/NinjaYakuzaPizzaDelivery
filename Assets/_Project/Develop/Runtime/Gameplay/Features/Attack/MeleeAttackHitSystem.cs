e8588e1d87039518495aeb22162557f99555ae6c
LuQmu5 <luqdev1007@gmail.com>
2026-03-30 01:13:31 +0400

    idealno

--- Assets/_Project/Develop/Runtime/Gameplay/Features/Attack/MeleeAttackHitSystem.cs
+++ Assets/_Project/Develop/Runtime/Gameplay/Features/Attack/MeleeAttackHitSystem.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 using Assets._Project.Develop.Runtime.Gameplay.EntitiesCore.Systems;
 using Assets._Project.Develop.Runtime.Gameplay.EntitiesCore.Mono;
 using Assets._Project.Develop.Runtime.Gameplay.Features.ApplyDamage;
-using Assets._Project.Develop.Runtime.Utilites.CoroutinesManagment; // Добавили для корутин
+using Assets._Project.Develop.Runtime.Utilites.CoroutinesManagment;
 using System;
 using System.Collections;
 using Assets._Project.Develop.Runtime.Gameplay.Features.LifeCycle;
@@ -13,20 +13,14 @@ namespace Assets._Project.Develop.Runtime.Gameplay.Features.Attack
 {
     public class MeleeAttackHitSystem : IInitializableSystem, IDisposableSystem
     {
-        private ReactiveEvent _attackDelayEndEvent;
-        private ReactiveVariable<float> _attackRange;
-        private ReactiveVariable<float> _attackDamage;
-        private ReactiveVariable<bool> _isGrounded;
-        private Rigidbody2D _rigidbody;
-        private Transform _transform;
+        private Entity _entity;
         private IDisposable _attackDelayEndDisposable;
-
         private readonly LayerMask _enemyMask;
         private readonly float _hitBounceForce;
-        private readonly ICoroutinesPerformer _coroutines; // Сервис для остановки времени
+        private readonly ICoroutinesPerformer _coroutines;
 
-        private const float HitStopDuration = 0.08f; // Оптимально для быстрого экшена
-        private const float HitStopScale = 0.05f;   // Почти полная остановка
+        private const float HitStopDuration = 0.15f;
+        private const float HitStopScale = 0.05f;
 
         public MeleeAttackHitSystem(LayerMask enemyMask, float hitBounceForce, ICoroutinesPerformer coroutines)
         {
@@ -37,78 +31,69 @@ namespace Assets._Project.Develop.Runtime.Gameplay.Features.Attack
 
         public void OnInit(Entity entity)
         {
-            _attackDelayEndEvent = entity.AttackDelayEndEvent;
-            _attackRange = entity.AttackRange;
-            _attackDamage = entity.AttackDamage;
-            _isGrounded = entity.IsGrounded;
-            _rigidbody = entity.Rigidbody;
-            _transform = entity.Transform;
-            _attackDelayEndDisposable = _attackDelayEndEvent.Subscribe(OnAttackDelayEnd);
+            _entity = entity;
+            _attackDelayEndDisposable = _entity.AttackDelayEndEvent.Subscribe(OnAttackHit);
         }
 
-        public void OnDispose() => _attackDelayEndDisposable?.Dispose();
-
-        private void OnAttackDelayEnd()
+        private void OnAttackHit()
         {
-            float direction = _transform.localScale.x > 0 ? 1f : -1f;
-            Vector2 origin = _transform.position;
-            Vector2 attackDirection = Vector2.right * direction;
-
+            float dir = _entity.Transform.localScale.x > 0 ? 1f : -1f;
             Collider2D[] hits = Physics2D.OverlapCircleAll(
-                origin + attackDirection * (_attackRange.Value * 0.5f),
-                _attackRange.Value * 0.5f,
-                _enemyMask);
+                (Vector2)_entity.Transform.position + Vector2.right * dir * (_entity.AttackRange.Value * 0.5f),
+                _entity.AttackRange.Value * 0.5f, _enemyMask);
 
             if (hits.Length == 0) return;
 
-            bool hitAnyEnemy = false;
-
-            foreach (Collider2D hit in hits)
+            bool hitAny = false;
+            foreach (var hit in hits)
             {
-                var monoEntity = hit.GetComponentInParent<MonoEntity>();
-                if (monoEntity != null)
+                var mono = hit.GetComponentInParent<MonoEntity>();
+                if (mono != null)
                 {
-                    ApplyDamageToTarget(monoEntity.LinkedEntity, hit.transform.position);
-                    hitAnyEnemy = true;
+                    ApplyDamage(mono.LinkedEntity, hit.transform.position);
+                    hitAny = true;
                 }
             }
 
-            if (hitAnyEnemy)
+            if (hitAny)
             {
-                ApplyDmcJuggle();
-                _coroutines.StartPerform(DoHitStop()); // Запускаем "стоп-кадр"
+                ApplyJuggle();
+                ExtendInvulnerability();
+                _coroutines.StartPerform(DoHitStop());
+            }
+        }
+
+        private void ApplyDamage(Entity target, Vector2 pos)
+        {
+            if (target.HasComponent<CurrentHealth>())
+            {
+                target.CurrentHealth.Value -= _entity.AttackDamage.Value;
+                target.TakeDamageEvent?.Invoke(new DamageData { Amount = _entity.AttackDamage.Value, SourcePosition = pos });
+            }
+        }
+
+        private void ApplyJuggle()
+        {
+            float jump = _entity.IsGrounded.Value ? _hitBounceForce * 0.5f : _hitBounceForce;
+            _entity.Rigidbody.linearVelocity = new Vector2(_entity.Rigidbody.linearVelocity.x, Mathf.Max(0, _entity.Rigidbody.linearVelocity.y) + jump);
+        }
+
+        private void ExtendInvulnerability()
+        {
+            if (_entity.HasComponent<AttackInvulnerabilityTimer>())
+            {
+                _entity.AttackInvulnerabilityTimer.Value = _entity.AttackInvulnerabilityDuration.Value;
+                _entity.IsAttackInvulnerable.Value = true;
             }
         }
 
         private IEnumerator DoHitStop()
         {
             Time.timeScale = HitStopScale;
-            // Используем WaitForSecondsRealtime, потому что обычный таймер замедлен
             yield return new WaitForSecondsRealtime(HitStopDuration);
             Time.timeScale = 1f;
         }
 
-        private void ApplyDamageToTarget(Entity target, Vector2 hitPoint)
-        {
-            if (target.HasComponent<CurrentHealth>())
-            {
-                float damage = _attackDamage.Value;
-                target.CurrentHealth.Value -= damage;
-                target.TakeDamageEvent?.Invoke(new DamageData { Amount = damage, SourcePosition = hitPoint });
-            }
-        }
-
-        private void ApplyDmcJuggle()
-        {
-            if (!_isGrounded.Value)
-            {
-                float newYVelocity = Mathf.Max(_rigidbody.linearVelocity.y, 0) + _hitBounceForce;
-                _rigidbody.linearVelocity = new Vector2(_rigidbody.linearVelocity.x, newYVelocity);
-            }
-            else
-            {
-                _rigidbody.linearVelocity = new Vector2(_rigidbody.linearVelocity.x, _hitBounceForce * 0.4f);
-            }
-        }
+        public void OnDispose() => _attackDelayEndDisposable?.Dispose();
     }
 }
